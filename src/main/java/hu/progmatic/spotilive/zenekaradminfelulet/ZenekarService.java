ceda810178e6c4dfec19c759712cf4b48cd1e64b
kuglert <kugler.tamas2@gmail.com>
2022-06-27 13:07:36 +0200

    projekt

---
+++ src/main/java/hu/progmatic/spotilive/zenekaradminfelulet/ZenekarService.java
@@ -0,0 +1,11 @@
+package hu.progmatic.spotilive.zenekaradminfelulet;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
+public class ZenekarService {
+
+}
---
+++ src/test/java/hu/progmatic/spotilive/zenekaradminfelulet/ZenekarServiceTest.java
@@ -0,0 +1,19 @@
+package hu.progmatic.spotilive.zenekaradminfelulet;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ZenekarServiceTest {
+
+    @Autowired
+    ZenekarService zenekarService;
+
+    @Test
+    @Disabled
+    void createZenekar() {
+    }
+}
