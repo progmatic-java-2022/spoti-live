1e6210f79a73ac66004e79ee66ad9480c357c33c
csakos05 <csakos05@gmail.com>
2022-06-28 12:52:12 +0200

    zenekarAdmin

--- src/main/java/hu/progmatic/spotilive/zenekar/Zenekar.java
+++ src/main/java/hu/progmatic/spotilive/zenekar/Zenekar.java
@@ -3,6 +3,7 @@ package hu.progmatic.spotilive.zenekar;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -15,11 +16,12 @@ public class Zenekar {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    @NotBlank
+
     @Column(unique = true)
     private String nev;
     @Column(unique = true)
-    @NotBlank
+//    @NotBlank
+//    @Email(regexp = ".*@.*",message = "miiii")
     private String email;
     @Column(unique = true)
     private String telefonszam;
