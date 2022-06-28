7decb0406d4ea3ab8b3525ee77670ad642acf797
kuglert <kugler.tamas2@gmail.com>
2022-06-28 11:18:07 +0200

    Zenekar karbantartás

--- src/main/java/hu/progmatic/spotilive/zenekar/ZenekarAdminController.java
+++ src/main/java/hu/progmatic/spotilive/zenekar/ZenekarAdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,12 @@ public class ZenekarAdminController {
     @PostMapping("/zenekarKarbantartas/zenekar/")
     public String addZenekar (Model model, @ModelAttribute("zenekarPeldany") @Valid ZenekarDto dto,
                               BindingResult bindingResult){
+        if (!dto.getTelefonszam()
+                .matches("(?<elotag>\\+36[\\d]{2})?" +
+                        "(?<masikelotag>06[\\d]{2})?(?<elvalaszto1>[\\-\\/])?" +
+                        "([\\d]{7})?(?<utotagkotojellel>[\\d]{3}-[\\d]{4})"))
+            bindingResult.addError(new FieldError("zenekarPeldany", "telefonszam","Helyes formátum pl.: 0630-164-1922"));
+
         if (!bindingResult.hasErrors()){
            var ujZenekar = zenekarService.createZenekar(dto);
            model.addAttribute("zenekarPeldany", ujZenekar);
