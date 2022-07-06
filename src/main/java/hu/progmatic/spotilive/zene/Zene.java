f257e7d0f2f2cff7a2c93d0372754ef0bb61b570
HorvathMartonAntal <marton.horvath302@gmail.com>
2022-07-06 09:58:40 +0200

    Esemenyek admin felület #2

--- src/main/java/hu/progmatic/spotilive/zene/Zene.java
+++ src/main/java/hu/progmatic/spotilive/zene/Zene.java
@@ -3,6 +3,9 @@ package hu.progmatic.spotilive.zene;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +19,11 @@ public class Zene {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NotEmpty
     private String cim;
+    @NotEmpty
     private String eloado;
+    @NotNull
     private Integer hosszMp;
     @Builder.Default
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "zene")
--- src/main/java/hu/progmatic/spotilive/zene/ZeneDto.java
+++ src/main/java/hu/progmatic/spotilive/zene/ZeneDto.java
@@ -3,14 +3,19 @@ package hu.progmatic.spotilive.zene;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Builder
 @Data
 public class ZeneDto {
     private Integer id;
+    @NotEmpty
     private String cim;
+    @NotEmpty
     private String eloado;
+    @NotNull
     private Integer hosszMp;
     private List<String> tagStringList;
 
