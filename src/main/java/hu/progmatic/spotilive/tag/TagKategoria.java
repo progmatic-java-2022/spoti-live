7d0147d207f9d94b943ecabeedba087433fcd546
kuglert <kugler.tamas2@gmail.com>
2022-07-08 13:15:47 +0200

    Zenekar karbantartás

--- src/main/java/hu/progmatic/spotilive/tag/TagKategoria.java
+++ src/main/java/hu/progmatic/spotilive/tag/TagKategoria.java
@@ -1,9 +1,24 @@
 package hu.progmatic.spotilive.tag;
 
+import lombok.Data;
+import lombok.Value;
+
+import java.util.List;
+
 public enum TagKategoria {
 
-    MUFAJ,
-    TEMPO,
-    HANGULAT
+    MUFAJ("Műfaj"),
+    TEMPO("Tempó"),
+    HANGULAT("Hangulat");
+
+     private final String tagKategoriaString;
+
+    TagKategoria(String tagKategoriaNev){
+        tagKategoriaString = tagKategoriaNev;
+    }
+
+    public String getTagKategoriaString(){
+        return tagKategoriaString;
+    }
 
 }
