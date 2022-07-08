8200f5997db01031ebcaa38783e7067ce7f7b56e
ynagyandrea <y.nagy.andrea@gmail.com>
2022-07-08 14:47:20 +0200

    zene to zenekar szerviz

--- src/main/java/hu/progmatic/spotilive/esemeny/EsemenyTracklistController.java
+++ src/main/java/hu/progmatic/spotilive/esemeny/EsemenyTracklistController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,11 +24,22 @@ public class EsemenyTracklistController {
        model.addAttribute("esemenytracklist", esemenyService.getEsemenyZenei(esemenyid));
         return "/esemenytracklist";
     }
+    @PostMapping("/zenelista/{zeneId}/esemeny/{esemenyId}")
+    public String addSzavazat(@PathVariable ("zeneId") Integer zeneId,
+                              @PathVariable ("esemenyId") Integer esemenyId){
+        esemenyService.addSzavazat(AddSzavazatCommand.builder()
+                .esemenyId(esemenyId)
+                .zeneId(zeneId)
+                .build());
+        return "/zenelista/esemeny/{esemenyid}";
+    }
+
 
     @ModelAttribute("esemenytracklist")
     public List<ZeneToEsemenyDto> esemenyZenei(){
         return new ArrayList<>();
     }
+
 
 
 }
