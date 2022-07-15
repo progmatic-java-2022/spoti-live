1680daf8a762e32528a46f621f83fa01664157f6
Sárközy Ádám <sarkozyadam@gmail.com>
2022-07-15 13:07:01 +0200

    Tesztek átbeszélése

--- src/test/java/hu/progmatic/spotilive/esemeny/EsemenyControllerTest.java
+++ src/test/java/hu/progmatic/spotilive/esemeny/EsemenyControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -43,7 +42,7 @@ class EsemenyControllerTest {
   }
 
   @Test
-  @WithMockUser
+  @WithUserDetails(ADMIN_FELHASZNALO)
   void oldalbetolteseTest() throws Exception {
     oldalBetolteseSzovegLatszik("Demo esemény");
   }
