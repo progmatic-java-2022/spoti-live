35d4cfe27852ad67c6ad4c4dea334f0ce2f38235
Laszlo Tamas <2varadi.attila@gmail.com>
2022-07-19 11:40:24 +0200

    Guest felület létrehozása

--- src/main/java/hu/progmatic/spotilive/kezdolap/KezdolapController.java
+++ src/main/java/hu/progmatic/spotilive/kezdolap/KezdolapController.java
@@ -1,12 +1,33 @@
 package hu.progmatic.spotilive.kezdolap;
 
+import hu.progmatic.spotilive.felhasznalo.FelhasznaloService;
+import hu.progmatic.spotilive.felhasznalo.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+
+
 @Controller
 public class KezdolapController {
-  @RequestMapping("/")
-  public String kezdolap() {
-    return "index";
-  }
+
+    @Autowired
+    private FelhasznaloService felhasznaloService;
+
+    @RequestMapping("/")
+    public String kezdolap() {
+        if (
+                felhasznaloService.hasRole(UserType.Roles.USER_WRITE_ROLE) ||
+                felhasznaloService.hasRole(UserType.Roles.USER_READ_ROLE) ||
+                felhasznaloService.hasRole(UserType.Roles.ZENEKAR_KEZELES_ROLE) ||
+                felhasznaloService.hasRole(UserType.Roles.ESEMENY_KEZELES_ROLE) ||
+                felhasznaloService.hasRole(UserType.Roles.ADMIN_ROLE)) {
+
+            return "index";
+        } else {
+            return "guestindex";
+        }
+    }
 }
