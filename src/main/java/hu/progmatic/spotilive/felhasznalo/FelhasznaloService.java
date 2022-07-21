0380245120bd1f7ae56c7a90f6d246ebdce7a802
Sárközy Ádám <sarkozyadam@gmail.com>
2022-07-21 16:30:36 +0200

    Fake authentication handler

--- src/main/java/hu/progmatic/spotilive/felhasznalo/FelhasznaloService.java
+++ src/main/java/hu/progmatic/spotilive/felhasznalo/FelhasznaloService.java
@@ -13,6 +13,8 @@ import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+import static hu.progmatic.spotilive.demo.DemoService.ADMIN_FELHASZNALO;
+
 @Service
 @Log
 @Transactional
@@ -153,5 +155,13 @@ public class FelhasznaloService {
 
   public Felhasznalo findByUUid(String uuid) {
     return felhasznaloRepository.findFelhasznaloByUuidEquals(uuid);
+  }
+
+  public void createAlapFelhasznalok() {
+    if (felhasznaloRepository.count() == 0) {
+      add(new UjFelhasznaloCommand(ADMIN_FELHASZNALO, "adminpass", UserType.ADMIN, null));
+      add(new UjFelhasznaloCommand("user", "user", UserType.USER, null));
+      add(new UjFelhasznaloCommand("guest", "guest", UserType.GUEST, null));
+    }
   }
 }
