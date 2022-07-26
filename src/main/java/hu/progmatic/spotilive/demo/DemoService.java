0afc8f45e0163bb102fac7ec0005da7c269a4256
Laszlo Tamas <2varadi.attila@gmail.com>
2022-07-26 10:49:19 +0200

    taglista modalok megoldása

--- src/main/java/hu/progmatic/spotilive/demo/DemoService.java
+++ src/main/java/hu/progmatic/spotilive/demo/DemoService.java
@@ -40,7 +40,7 @@ public class DemoService {
     public static final String PREFIX1 = "1_zenekar_";
     public static final String PREFIX2 = "2_zenekar_";
     @Autowired
-    ZenekarService zenekarService;
+    private ZenekarService zenekarService;
     @Autowired
     private EsemenyService esemenyService;
     @Autowired
