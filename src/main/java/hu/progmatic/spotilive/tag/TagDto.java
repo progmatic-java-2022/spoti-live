06d0d4ee8e211cc1cab7f5cc836bdeff0c8ad766
kuglert <kugler.tamas2@gmail.com>
2022-07-28 11:13:59 +0200

    Zenekar karbantartás

--- src/main/java/hu/progmatic/spotilive/tag/TagDto.java
+++ src/main/java/hu/progmatic/spotilive/tag/TagDto.java
@@ -19,20 +19,13 @@ public class TagDto {
     private String tagNev;
     @NotNull(message = "Kategória megadása kötelező!")
     private TagKategoria tagKategoria;
-    private List<ZeneDto> zeneDtoList;
 
     public static TagDto factory(Tag tag) {
         return TagDto.builder()
                 .id(tag.getId())
                 .tagNev(tag.getTagNev())
-                .zeneDtoList(getZeneDtoList(tag))
                 .tagKategoria(tag.getTagKategoria())
                 .build();
     }
 
-    private static List<ZeneDto> getZeneDtoList(Tag tag) {
-       List<Zene> zenek = tag.getTagToZeneEntityList().stream().map(TagToZene::getZene).toList();
-       return zenek.stream().map(ZeneDto::factory).toList();
-
-    }
 }
